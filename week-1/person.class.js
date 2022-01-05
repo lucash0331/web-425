fd53116faf7acef897797981f1c1f3a80bdacbd9
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2022-01-05 10:52:11 -0500

    Adding assignment 1.4

---
+++ week-1/person.class.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+let myName = new Person("Lucas", "Hoffman");
+console.log('My name is ${myName.firstName ${myName.lastName}');
---
+++ week-1/person.class.ts
@@ -0,0 +1,17 @@
+import { IPerson } from "./person.interface";
+
+class Person implements IPerson {
+
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+
+let myName = new Person("Lucas", "Hoffman");
+console.log('My name is ${myName.firstName ${myName.lastName}');
+
