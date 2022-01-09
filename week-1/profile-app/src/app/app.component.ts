1cfa946510d9e155941142cf71f0c3b07edd31fe
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2022-01-09 16:14:18 -0500

    Adding assignment 1.5

--- week-1/profile-app/src/app/app.component.ts
+++ week-1/profile-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  myWorld = "You are now in Lucas Hoffman's world!"
+  assignment: string = 'Assignment 1.5 - Components';
 }
